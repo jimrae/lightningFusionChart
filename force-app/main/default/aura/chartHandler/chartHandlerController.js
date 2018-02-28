1b6f02c6c45e60c308e3ef6fb01c6d1997be9931
Jim Rae <jrae@xede.com>
2018-02-28 13:05:05 -0500

    Initial Commit

---
+++ force-app/main/default/aura/chartHandler/chartHandlerController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jimrae on 2/26/18.
+ */
+({
+    afterScriptsLoaded: function(component,event,helper){
+        helper.makeChart(component,event,helper);
+    },
+    doInit: function(component, event, helper){
+
+    }
+})
---
+++ force-app/main/default/aura/chartTester/chartTester.app
@@ -0,0 +1,7 @@
+<!--
+ - Created by jimrae on 2/26/18.
+ -->
+
+<aura:application description="chartTester">
+    <c:chartHandler />
+</aura:application>
